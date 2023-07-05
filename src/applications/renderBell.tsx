e2dead269f94cb161cee25f2ab374721662ccddc
Federico Hoerth <fhoerth@matterway.io>
2023-07-05 13:41:55 -0300

    Fix styling issues

--- src/applications/renderBell.tsx
+++ src/applications/renderBell.tsx
@@ -1,6 +1,6 @@
-import bell from 'data-text:./Bell/styles.css';
+import bellStyleText from 'data-text:./Bell/styles.css';
 import twBaseText from 'data-text:~base.css';
-import tWstyleText from 'data-text:~style.css';
+import tWStyleText from 'data-text:~style.css';
 import { createRoot } from 'react-dom/client';
 
 import type { ContentCommunicationChannel } from '~communication-channel';
@@ -11,7 +11,7 @@ import { CommunicationChannelContextProvider } from './Context';
 
 export function renderBell(channel: ContentCommunicationChannel): void {
   const root = createRoot(
-    createGoogleComBellShadowRoot([twBaseText, tWstyleText]),
+    createGoogleComBellShadowRoot([bellStyleText, twBaseText, tWStyleText]),
   );
 
   root.render(
