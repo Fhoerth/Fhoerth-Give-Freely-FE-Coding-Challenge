ce98b48737360b52cbfa39f228bd81e22c5eae1f
Federico Hoerth <fhoerth@matterway.io>
2023-07-05 13:31:30 -0300

    Add bell icon styles.

--- src/applications/renderBell.tsx
+++ src/applications/renderBell.tsx
@@ -1,5 +1,6 @@
-import baseText from 'data-text:~base.css';
-import styleText from 'data-text:~style.css';
+import bell from 'data-text:./Bell/styles.css';
+import twBaseText from 'data-text:~base.css';
+import tWstyleText from 'data-text:~style.css';
 import { createRoot } from 'react-dom/client';
 
 import type { ContentCommunicationChannel } from '~communication-channel';
@@ -9,7 +10,9 @@ import { Bell } from './Bell';
 import { CommunicationChannelContextProvider } from './Context';
 
 export function renderBell(channel: ContentCommunicationChannel): void {
-  const root = createRoot(createGoogleComBellShadowRoot([baseText, styleText]));
+  const root = createRoot(
+    createGoogleComBellShadowRoot([twBaseText, tWstyleText]),
+  );
 
   root.render(
     <CommunicationChannelContextProvider channel={channel}>
