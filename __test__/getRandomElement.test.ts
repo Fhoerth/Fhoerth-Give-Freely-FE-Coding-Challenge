0ecb60af0bd413bd17d63169199af27ac63a8b2e
fhoerth <federicohoerth@gmail.com>
2023-07-10 18:29:10 -0300

    Add getRandomElement edge case (#35)
    * Add middle edge case
    
    * Update README.md
    
    ---------
    
    Co-authored-by: Federico Hoerth <federicohoerth@gmail.com>

--- __test__/getRandomElement.test.ts
+++ __test__/getRandomElement.test.ts
@@ -16,6 +16,19 @@ describe('getRandomElement', () => {
     });
   });
 
+  describe('when Math.random() returns a number strictly between 0 and 1', () => {
+    it('should return intermediate element', () => {
+      jest.spyOn(global.Math, 'random').mockReturnValue(0.5);
+
+      const elements = [1, 2, 3, 4, 5];
+      const randomElement = getRandomElement(elements);
+
+      expect(randomElement).toBe(3);
+
+      jest.spyOn(global.Math, 'random').mockRestore();
+    });
+  });
+
   describe('when Math.random() returns a number close to 1', () => {
     it('should return the last element', () => {
       jest.spyOn(global.Math, 'random').mockReturnValue(1 - Number.EPSILON);
