5ccffe5aba6d19b6c046729ead252429f711625d
Federico Hoerth <fhoerth@matterway.io>
2023-07-10 18:23:27 -0300

    Add getRandomElement unit test

---
+++ __test__/getRandomElement.test.ts
@@ -0,0 +1,31 @@
+import { jest } from '@jest/globals';
+
+import { getRandomElement } from '../src/utils/getRandomElement';
+
+describe('getRandomElement', () => {
+  describe('when Math.random() returns 0', () => {
+    it('should return the first element', () => {
+      jest.spyOn(global.Math, 'random').mockReturnValue(0);
+
+      const elements = [1, 2, 3, 4, 5];
+      const randomElement = getRandomElement(elements);
+
+      expect(randomElement).toBe(1);
+
+      jest.spyOn(global.Math, 'random').mockRestore();
+    });
+  });
+
+  describe('when Math.random() returns a number close to 1', () => {
+    it('should return the last element', () => {
+      jest.spyOn(global.Math, 'random').mockReturnValue(1 - Number.EPSILON);
+
+      const elements = [1, 2, 3, 4, 5];
+      const randomElement = getRandomElement(elements);
+
+      expect(randomElement).toBe(5);
+
+      jest.spyOn(global.Math, 'random').mockRestore();
+    });
+  });
+});
