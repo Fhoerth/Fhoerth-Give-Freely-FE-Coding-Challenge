eb121316a42424e14911ad0548ff9475ccf1a716
Federico Hoerth <fhoerth@matterway.io>
2023-07-09 21:35:15 -0300

    Fix jsx errors

--- src/applications/modal/index.tsx
+++ src/applications/modal/index.tsx
@@ -97,9 +97,9 @@ export const Modal: React.FC<ModalProps> = ({ opened = false }) => {
               <svg className="w-3 h-3" fill="none" viewBox="0 0 14 14">
                 <path
                   stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
                   d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
                 />
               </svg>
