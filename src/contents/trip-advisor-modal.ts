2d418dcc8f697b6f2b45486103308535996dee36
Federico Hoerth <fhoerth@matterway.io>
2023-07-21 16:09:07 -0300

    Remove request

--- src/contents/trip-advisor-modal.ts
+++ src/contents/trip-advisor-modal.ts
@@ -3,7 +3,6 @@ import type { PlasmoCSConfig } from 'plasmo';
 import { renderModal } from '~applications/trip-advisor-modal/renderModal';
 import { ContentCommunicationChannel } from '~communication-channel/channels/ContentCommunicationChannel';
 import { ChannelId, Client, MessageType } from '~communication-channel/enums';
-import type { OpenExternalLinkRequest } from '~communication-channel/types';
 import { onDomContentLoaded } from '~utils/onDomContentLoaded';
 
 export const config: PlasmoCSConfig = {
@@ -18,12 +17,6 @@ async function domContentLoaded(): Promise<void> {
   });
 
   await channel.initialize();
-
-  // const request: OpenExternalLinkRequest = {
-  //   type: MessageType.OPEN_EXTERNAL_LINK,
-  //   channelId: ChannelId,
-  // };
-  // await channel.sendToBackground<OpenExternalLinkRequest, any>(request);
 
   renderModal(channel);
 }
